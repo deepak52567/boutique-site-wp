f25b2bcac44b82cab85e320f2e36f5b0e2adb845
Deepak Kumar <deepak52567@gmail.com>
2019-01-13 22:07:48 +0530

    changes in id

--- main-intro.php
+++ main-intro.php
@@ -1,6 +1,18 @@
 <?php $topPage = get_field('top_title'); ?>
 <section class="top-main">
-    <i class="fas fa-shopping-cart cart-art"></i>
+    <div class="pattern-con">
+        <div class="left-pt-1"></div>
+        <div class="left-pt-2"></div>
+        <i class="fas fa-shopping-cart cart-art"></i>
+        <div class="bag-con">
+            <i class="fas fa-shopping-bag bag-art"></i>
+        </div>
+        <div class="gem-con">
+            <i class="fas fa-gem"></i>
+            <i class="fas fa-gem"></i>
+            <i class="fas fa-gem"></i>
+        </div>
+    </div>
     <div class="head">
         <h1><?php echo $topPage['title']; ?></h1>
         <h5><?php echo $topPage['location_title']; ?></h5>
